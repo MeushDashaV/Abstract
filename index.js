773c44d36fefbd87884f500c1070d757cd442c16
MeushDashaV <meyshuha@gmail.com>
2024-08-29 14:04:21 +0300

    added @media for reviews section

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const prevButton = document.querySelector('.carousel-button-left');
-  const nextButton = document.querySelector('.carousel-button-right');
   const list = document.querySelector('.reviews-section__list');
   const items = document.querySelectorAll('.reviews-section__item');
-  const itemsCount = items.length;
-  let index = 0; // Початковий індекс
+  const prevButton = document.querySelector('.carousel-button-left');
+  const nextButton = document.querySelector('.carousel-button-right');
+  let currentIndex = 0;
 
-  // Функція для оновлення позиції списку
-  const updateListPosition = () => {
-    const offset = -index * 352; // Ширина картки
-    list.style.transform = `translateX(${offset}px)`;
-  };
+  function updateCarousel() {
+    // Зміна позиції слайда
+    list.style.transform = `translateX(-${currentIndex * 100}%)`;
+  }
 
-  // Перемикання карток вліво
   prevButton.addEventListener('click', () => {
-    index = index === 0 ? itemsCount - 1 : index - 1; // Повертається до останньої картки
-    updateListPosition();
+    currentIndex = currentIndex === 0 ? items.length - 1 : currentIndex - 1;
+    updateCarousel();
   });
 
-  // Перемикання карток вправо
   nextButton.addEventListener('click', () => {
-    index = index === itemsCount - 1 ? 0 : index + 1; // Повертається до першої картки
-    updateListPosition();
+    currentIndex = currentIndex === items.length - 1 ? 0 : currentIndex + 1;
+    updateCarousel();
   });
+
+  // Ініціалізація каруселі
+  updateCarousel();
 });
